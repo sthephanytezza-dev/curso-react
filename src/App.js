9d507c20e6b9c55a8ce609af2e0c1c815ddc9b68
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-06 14:48:37 -0300

    feat: renderização de listas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import Condicional from './components/Condicional';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import OutraLista from './components/OutraLista';
 
 
 function App() {
 
+  const meusItens = ['React', 'Vue', 'Angula'];
+
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
+      <h1>Renderização de listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens=''/>
     </div>
   );
 }
